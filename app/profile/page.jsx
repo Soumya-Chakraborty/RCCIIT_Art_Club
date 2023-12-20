470667b96ac567381843bb3d40ccdbdc986dd895
Soumya Chakraborty <soumyachakraborty198181@gmail.com>
2023-12-20 23:39:04 +0530

    UI enhanced

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -20,27 +20,24 @@ const page = () => {
       {loading ? (
         <Spinner />
       ) : user ? (
-        <div className="text-2xl mb-4 text-gray-600">
-          {/*
-            Using template literals for better readability and concatenation
-            Adding space after 'Hey' for better spacing
-          */}
-          {`Hey `}
-          <span className="text-red-500 font-bold">{user.displayName}</span>!!
-          🎨 Welcome to the excitement of the{" "}
-          <span className="font-bold">Animae Art Contest 2023</span>! 🚀 Your
-          registration is confirmed, and we're thrilled to have you on board.
-          {/*
-            Adding margin-top for better separation
-            Adjusting button styling for consistency
-          */}
+
+        <div className="profile-section text-gray-800 bg-white p-8 rounded-lg shadow-md">
+          <h1 className="text-4xl font-extrabold mb-6 text-indigo-600">
+            Welcome, <span className="text-red-500">{user.displayName}</span>!
+          </h1>
+          <p className="text-lg mb-8 leading-relaxed">
+            🎨 Dive into the excitement of the{" "}
+            <span className="font-bold text-indigo-600">Animae Art Contest 2023</span>!
+            Your registration is confirmed, and we're thrilled to have you on board.
+          </p>
           <a
             href="https://forms.gle/WF9o826QLsfhm9nY6"
-            className="mt-4 inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full transition-all duration-300 ease-in-out"
+            className="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-full transition-all duration-300 ease-in-out"
           >
             Submit Your Painting
           </a>
         </div>
+
 
       ) : (
         <p className="text-lg text-gray-700">
@@ -48,7 +45,7 @@ const page = () => {
         </p>
       )}
     </div>
-  );  
+  );
 };
 
 export default page;
